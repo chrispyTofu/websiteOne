2e8291eb9bbbe3908d38267ef94e2bcef0ddf7dc
Chris <cau19@myseneca.ca>
2018-05-31 12:28:11 -0400

    c

--- p/assets/js/main.js
+++ p/assets/js/main.js
@@ -1,21 +1,45 @@
 //navBar hide on scroll
 var prevScrollpos = window.pageYOffset;
+var small = window.matchMedia("(max-width: 900px)");
+
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   var nav = document.getElementById("navbar");
   if (prevScrollpos > currentScrollPos) {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      nav.style.backgroundColor = "rgba(178, 206, 222, 0.8)";
+      nav.style.backgroundColor = "#b2cede";  
       nav.style.transition = "all 0.5s";
     }
     else {
-      nav.style.backgroundColor = "transparent";
+      if (small.matches) {
+        nav.style.backgroundColor = "#b2cede";
+      }
+      else {
+        nav.style.backgroundColor = "transparent";
+      }
     }
     nav.style.top = "0";
   } else {
-    nav.style.top = "-200px";
+    if (small.matches) {
+      nav.style.top = "-500px";
+    }
+    else {
+      nav.style.top = "-200px";
+    }
   }
   prevScrollpos = currentScrollPos;
 }
+window.onload = function() {
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function() {
+    console.log("hello");
+    hamburger.classList.toggle("is-active");
+  });
+}
+
+
+
+
+
 
 
